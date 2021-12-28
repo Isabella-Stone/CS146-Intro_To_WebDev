97171675d6f2f5415412667f7bf346e997595994
Isabella Stone <52970853+Isabella-Stone@users.noreply.github.com>
2021-12-28 17:02:56 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,162 @@
+// JavaScript source code
+// I pledge my honor that I have abided by the Stevens Honor System.
+function pep111() {
+        var pep111lec = document.getElementById('pep111lec')
+        var pep111x = document.getElementsByClassName('PEP111')
+        var pep111color = document.getElementById('PEP111color').value
+
+        if (pep111lec.innerHTML == '') {
+            for (i=0; i<pep111x.length; i++) {
+                pep111x[i].innerHTML = 'PEP111'
+                pep111x[i].style.backgroundColor = pep111color
+            }
+        } 
+        else {
+            for (i=0; i<pep111x.length; i++) {
+                pep111x[i].innerHTML = ''
+                pep111x[i].style.backgroundColor = "white"
+            }
+        }
+    }
+
+function pep111Color() {
+        var pep111lec = document.getElementById('pep111lec')
+        var pep111x = document.getElementsByClassName('PEP111')
+        var pep111color = document.getElementById('PEP111color').value
+
+        if (pep111lec.innerHTML != ''){
+            for (i=0; i<pep111x.length; i++) {
+            pep111x[i].style.backgroundColor = pep111color
+        }
+        }
+        
+         
+    }
+
+function cs115() {
+        var cs115lec = document.getElementById('cs115lec')
+        var cs115x = document.getElementsByClassName('CS115')
+        var cs115color = document.getElementById('CS115color').value
+
+        if (cs115lec.innerHTML == '') {
+            for (i=0; i<cs115x.length; i++) {
+                cs115x[i].innerHTML = 'CS115'
+                cs115x[i].style.backgroundColor = cs115color
+            }
+        } 
+        else {
+            for (i=0; i<cs115x.length; i++) {
+                cs115x[i].innerHTML = ''
+                cs115x[i].style.backgroundColor = "white"
+            }
+        }
+    }
+
+function cs115Color() {
+        var cs115lec = document.getElementById('cs115lec')
+        var cs115x = document.getElementsByClassName('CS115')
+        var cs115color = document.getElementById('CS115color').value
+
+        if (cs115lec.innerHTML != ''){
+            for (i=0; i<cs115x.length; i++) {
+            cs115x[i].style.backgroundColor = cs115color
+        }
+        }
+         
+    }
+
+function cal103() {
+        var cal103lec = document.getElementById('cal103lec')
+        var cal103x = document.getElementsByClassName('CAL103')
+        var cal103color = document.getElementById('CAL103color').value
+
+        if (cal103lec.innerHTML == '') {
+            for (i=0; i<cal103x.length; i++) {
+                cal103x[i].innerHTML = 'CAL103'
+                cal103x[i].style.backgroundColor = cal103color
+            }
+        } 
+        else {
+            for (i=0; i<cal103x.length; i++) {
+                cal103x[i].innerHTML = ''
+                cal103x[i].style.backgroundColor = "white"
+            }
+        }
+    }
+
+function cal103Color() {
+        var cal103lec = document.getElementById('cal103lec')
+        var cal103x = document.getElementsByClassName('CAL103')
+        var cal103color = document.getElementById('CAL103color').value
+
+        if (cal103lec.innerHTML != ''){
+            for (i=0; i<cal103x.length; i++) {
+            cal103x[i].style.backgroundColor = cal103color
+        }
+        }
+         
+    }
+
+function cs146() {
+        var cs146lec = document.getElementById('cs146lec')
+        var cs146x = document.getElementsByClassName('CS146')
+        var cs146color = document.getElementById('CS146color').value
+
+        if (cs146lec.innerHTML == '') {
+            for (i=0; i<cs146x.length; i++) {
+                cs146x[i].innerHTML = 'CS146'
+                cs146x[i].style.backgroundColor = cs146color
+            }
+        } 
+        else {
+            for (i=0; i<cs146x.length; i++) {
+                cs146x[i].innerHTML = ''
+                cs146x[i].style.backgroundColor = "white"
+            }
+        }
+    }
+
+function cs146Color() {
+    var cs146lec = document.getElementById('cs146lec')
+    var cs146x = document.getElementsByClassName('CS146')
+    var cs146color = document.getElementById('CS146color').value
+
+    if (cs146lec.innerHTML != ''){
+            for (i=0; i<cs146x.length; i++) {
+            cs146x[i].style.backgroundColor = cs146color
+        }
+     }
+         
+}
+
+function ma121() {
+        var ma121lec = document.getElementById('ma121lec')
+        var ma121x = document.getElementsByClassName('MA121')
+        var ma121color = document.getElementById('MA121color').value
+
+        if (ma121lec.innerHTML == '') {
+            for (i=0; i<ma121x.length; i++) {
+                ma121x[i].innerHTML = 'MA121'
+                ma121x[i].style.backgroundColor = ma121color
+            }
+        } 
+        else {
+            for (i=0; i<ma121x.length; i++) {
+                ma121x[i].innerHTML = ''
+                ma121x[i].style.backgroundColor = "white"
+            }
+        }
+    }
+
+function ma121Color() {
+    var ma121lec = document.getElementById('ma121lec')
+    var ma121x = document.getElementsByClassName('MA121')
+    var ma121color = document.getElementById('MA121color').value
+
+    if (ma121lec.innerHTML != ''){
+            for (i=0; i<ma121x.length; i++) {
+            ma121x[i].style.backgroundColor = ma121color
+        }
+     }
+         
+}
